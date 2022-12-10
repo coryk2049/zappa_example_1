2a1515d6a185aaf3a74511ceced5f05e171d4035
coryk2049 <cory.kin@gmail.com>
2022-12-09 19:40:32 -0500

    Revisions

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello(event=None, context=None):
+    logger.info('Lambda function invoked index()')
+    return '<h1>Welcome to your first serverless Hello World!!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
