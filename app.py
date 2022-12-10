31dc354dc99ca7e01acb07133e509aa19205dbe7
coryk2049 <cory.kin@gmail.com>
2022-12-09 21:02:13 -0500

    Revisions

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ logger.setLevel(logging.DEBUG)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello(event=None, context=None):
-    logger.info('Lambda function invoked index()')
+    logger.info('Lambda function invoked index')
     return '<h1>Welcome to your first serverless Hello World!!</h1>'
 
 if __name__ == '__main__':
